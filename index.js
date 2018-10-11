676541d21c12214a4d7fa9c9ac2834659bec9974
marcoaimo <marcoaimo.am@gmail.com>
2018-10-11 18:58:04 +0200

    fix client reference

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 
 const SerialPort = require('serialport')
 
-let cerealPort
-let parser
-
 class CerealServer {
   constructor (port, baudRate, callback) {
-    cerealPort = new SerialPort(port, baudRate)
-    parser = cerealPort.pipe(new SerialPort.parsers.Readline())
+    this.client = new SerialPort(port, baudRate)
+    this.parser = this.client.pipe(new SerialPort.parsers.Readline())
     this.callback = callback
   }
   start () {
-    cerealPort.on('open', () => console.log('Open serial connection'))
-    cerealPort.on('close', () => console.log('Closed serial connection'))
-    cerealPort.on('error', (error) => console.log('Error on serial connection', error))
-    parser.on('data', this.callback)
+    this.client.on('open', () => console.log('Open serial connection', this.client.path))
+    this.client.on('close', () => {
+      console.log('Closed serial connection', this.client.path)
+      this.client = null
+    })
+    this.client.on('error', (error) => console.log('Error on serial connection', error))
+    this.parser.on('data', this.callback)
   }
   broadcast () {
   }
