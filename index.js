2c9d23ffe9ac61684c448e728a49ead54eb11997
marcoaimo <marcoaimo.am@gmail.com>
2018-10-11 15:46:01 +0200

    initial class structure

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import { SerialPort } from 'serialport'
+
+class CerealServer {
+  constructor () {
+  }
+  start () {
+  }
+  broadcast () {
+  }
+}
+
+module.exports = { CerealServer: CerealServer }
