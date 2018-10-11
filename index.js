1d4a70241f12556d5fc3e73e81d0539cbad1418c
marcoaimo <marcoaimo.am@gmail.com>
2018-10-11 17:34:27 +0200

    add callback to handle data

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ let cerealPort
 let parser
 
 class CerealServer {
-  constructor (port, baudRate) {
+  constructor (port, baudRate, callback) {
     cerealPort = new SerialPort(port, baudRate)
     parser = cerealPort.pipe(new SerialPort.parsers.Readline())
+    this.callback = callback
   }
   start () {
     cerealPort.on('open', () => console.log('Open serial connection'))
     cerealPort.on('close', () => console.log('Closed serial connection'))
     cerealPort.on('error', (error) => console.log('Error on serial connection', error))
-    parser.on('data', (data) => console.log(data))
+    parser.on('data', this.callback)
   }
   broadcast () {
   }
