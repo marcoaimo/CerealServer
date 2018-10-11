d16eb99a0b7c90b4d787c57b54b9700c087ac4a6
marcoaimo <marcoaimo.am@gmail.com>
2018-10-11 18:59:27 +0200

    update example with write function

--- example/ex1.js
+++ example/ex1.js
@@ -4,3 +4,7 @@ const { CerealServer } = require('../')
 
 const cereal = new CerealServer('/dev/tty.usbmodem1411', 9600, (data) => console.log(data))
 cereal.start()
+setTimeout(function () {
+  console.log('Write on serial')
+  cereal.broadcast('Hello world!')
+}, 5000)
