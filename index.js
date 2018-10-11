243fdbad4c1d0b1ccca122197c8fa918360a190f
marcoaimo <marcoaimo.am@gmail.com>
2018-10-11 17:09:12 +0200

    basic implementation

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 'use strict'
 
-import { SerialPort } from 'serialport'
+const SerialPort = require('serialport')
+
+let cerealPort
+let parser
 
 class CerealServer {
-  constructor () {
+  constructor (port, baudRate) {
+    cerealPort = new SerialPort(port, baudRate)
+    parser = cerealPort.pipe(new SerialPort.parsers.Readline())
   }
   start () {
+    cerealPort.on('open', () => console.log('Open serial connection'))
+    cerealPort.on('close', () => console.log('Closed serial connection'))
+    cerealPort.on('error', (error) => console.log('Error on serial connection', error))
+    parser.on('data', (data) => console.log(data))
   }
   broadcast () {
   }
