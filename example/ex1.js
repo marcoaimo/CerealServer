634adf7c5f583d59dbaa13fdbb21edcee4472f89
marcoaimo <marcoaimo.am@gmail.com>
2018-10-12 14:42:30 +0200

    update example

--- example/ex1.js
+++ example/ex1.js
@@ -2,9 +2,13 @@
 
 const { CerealServer } = require('../')
 
-const cereal = new CerealServer('/dev/tty.usbmodem1411', 9600, (data) => console.log(data))
+let cereal = new CerealServer('/dev/tty.usbmodem1411', 9600, (data) => console.log(data))
 cereal.start()
 setTimeout(function () {
   console.log('Write on serial')
   cereal.broadcast('Hello world!')
 }, 5000)
+setTimeout(function () {
+  console.log('Close connection')
+  cereal.stop()
+}, 10000)
