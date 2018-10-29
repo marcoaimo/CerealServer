cf4e53b9a56203cf05dc74968aaa3ef5689fc36d
marcoaimo <marcoaimo.am@gmail.com>
2018-10-29 15:52:45 +0100

    manage lock/unlock features on ports

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 'use strict'
 
 const SerialPort = require('serialport')
+const LOCKED_SERIAL_PORTS = []
+const _defaults = {
+  port: null,
+  baudRate: 9600
+}
+
+const _findPortByName = (comName) => {
+  if (LOCKED_SERIAL_PORTS <= 0) return null
+  let port = LOCKED_SERIAL_PORTS.find((p) => {
+    return p.comName = comName
+  })
+  return port
+}
 
 const _connectSerialPort = (server, ports) => {
   try {
     if (ports.length <= 0) throw new Error('No serial ports detected!')
-    let port = ports[0].comName
-    server.client = new SerialPort(port, server.baudRate)
+    let port = new CerealPort(ports[0])
+    if (port.locked) return
+    server.client = new SerialPort(port.comName, server.baudRate)
     server.parser = server.client.pipe(new SerialPort.parsers.Readline())
+    port.lock()
 
     server.client.on('open', () => console.log('Open serial connection', server.client.path))
     server.client.on('close', () => {
       console.log('Closed serial connection', server.client.path)
       server.client = null
+      port.unlock()
     })
     server.client.on('error', (error) => console.log('Error on serial connection', error))
     server.parser.on('data', server.callback)
@@ -21,17 +37,37 @@ const _connectSerialPort = (server, ports) => {
   }
 }
 
+class CerealPort {
+  constructor (port) {
+    let p = _findPortByName(port.comName)
+    if (p) return p
+    Object.assign(this, port)
+    this.locked = false
+  }
+  lock () {
+    if (this.locked) return
+    this.locked = true
+    LOCKED_SERIAL_PORTS.push(this)
+  }
+  unlock () {
+    if (!this.locked) return
+    let lockedPortIndex = LOCKED_SERIAL_PORTS.indexOf(this)
+    LOCKED_SERIAL_PORTS.splice(this, 1)
+    this.locked = false
+  }
+}
+
 class CerealServer {
-  constructor (port, baudRate, callback) {
+  constructor (options, callback) {
+    Object.assign(this, _defaults, options)
     this.client = null
-    this.baudRate = baudRate
     this.callback = callback
   }
   start () {
     let _serialPortsCheck = () => SerialPort.list().then((ports) => _connectSerialPort(this, ports))
     console.info('start cereal server for serial ports communication')
     _serialPortsCheck()
-    setInterval(_serialPortsCheck, 5000)
+    setInterval(_serialPortsCheck, 15000)
   }
   stop () {
     try {
